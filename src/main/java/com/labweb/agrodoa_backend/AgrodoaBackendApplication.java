70090ce5b8a2ca87686624327b4f451f809d03e8
let <leticiacostaoa@gmail.com>
2025-05-16 09:11:36 -0300

    creating hthe spring project

---
+++ src/main/java/com/labweb/agrodoa_backend/AgrodoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.labweb.agrodoa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrodoaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrodoaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labweb/agrodoa_backend/AgrodoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labweb.agrodoa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrodoaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
