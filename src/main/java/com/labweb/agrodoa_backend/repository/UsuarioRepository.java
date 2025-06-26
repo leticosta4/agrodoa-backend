1d3bf759f5bdf33335bfd86ad7a05c3575fdc137
let <leticiacostaoa@gmail.com>
2025-06-25 22:53:12 -0300

    fix(enumRelacaoBenef): atualizei os nomes dos tipos de relação que o beneficiario tem com um anuncio

--- src/main/java/com/labweb/agrodoa_backend/repository/UsuarioRepository.java
+++ src/main/java/com/labweb/agrodoa_backend/repository/UsuarioRepository.java
@@ -11,7 +11,7 @@ import com.labweb.agrodoa_backend.model.projection.UsuarioProjection;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
   boolean existsByIdAndTipoUsuario(Long id, Tipo tipoUsuario);
-  ArrayList <UsuarioProjection> findAllBy();  //para consulta pelo usuario
+  ArrayList <UsuarioProjection> findAllBy();  //para consulta pelo adm quando tiver -que gerenciar os usuarios
   UsuarioProjection findUsuarioById(Long usuarioId);  //dependendo do tipo que for ele puxa listas específicas la dentro do service
   void removeById(Long usuarioId);
 }
