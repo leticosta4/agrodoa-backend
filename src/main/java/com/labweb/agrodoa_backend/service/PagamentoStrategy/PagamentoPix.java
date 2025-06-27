afb5c23f56629780cfd8bab466fa1bee35c65691
Cainan <cainan.bas@gmail.com>
2025-06-26 23:41:54 -0300

    em andamento Observer notificacao

---
+++ src/main/java/com/labweb/agrodoa_backend/service/PagamentoStrategy/PagamentoPix.java
@@ -0,0 +1,10 @@
+package com.labweb.agrodoa_backend.service.PagamentoStrategy;
+
+public class PagamentoPix implements PagamentoStrategy {
+
+    @Override
+    public void pagar(double valor) {
+        //System.out.println("Pagamento realizado via PIX no valor de R$ " + valor);
+    }
+    
+}
---
+++ src/main/java/com/labweb/agrodoa_backend/service/PagamentoStrategy/PagamentoStrategy.java
@@ -0,0 +1,5 @@
+package com.labweb.agrodoa_backend.service.PagamentoStrategy;
+
+public interface PagamentoStrategy {
+    void pagar(double valor);
+}
