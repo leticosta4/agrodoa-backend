cd7989a87c2d7a353b41379083ab3fb13f213f53
let <leticiacostaoa@gmail.com>
2025-06-26 21:34:47 -0300

    discard: replace projections for DTOs

--- src/main/java/com/labweb/agrodoa_backend/repository/UsuarioRepository.java
+++ src/main/java/com/labweb/agrodoa_backend/repository/UsuarioRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.labweb.agrodoa_backend.model.Tipo;
 import com.labweb.agrodoa_backend.model.pessoas.Usuario;
-import com.labweb.agrodoa_backend.model.projection.UsuarioProjection;
+import com.labweb.agrodoa_backend.dto.UsuarioDTO;
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
   boolean existsByIdAndTipoUsuario(Long id, Tipo tipoUsuario);
-  ArrayList <UsuarioProjection> findAllBy();  //para consulta pelo adm quando tiver -que gerenciar os usuarios
-  UsuarioProjection findUsuarioById(Long usuarioId);  //dependendo do tipo que for ele puxa listas específicas la dentro do service
+  ArrayList <UsuarioDTO> findAllBy();  //para consulta pelo adm quando tiver -que gerenciar os usuarios
+  UsuarioDTO findUsuarioById(Long usuarioId);  //dependendo do tipo que for ele puxa listas específicas la dentro do service
   void removeById(Long usuarioId);
 }
