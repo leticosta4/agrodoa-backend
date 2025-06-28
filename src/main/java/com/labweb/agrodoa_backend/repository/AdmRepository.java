d3d4d178b5937c3fb8ac204611a2357b4269ac48
let <leticiacostaoa@gmail.com>
2025-06-27 22:54:41 -0300

    fix(repository): renomeando ids em adm e user para contaId > quebrava o projeto

--- src/main/java/com/labweb/agrodoa_backend/repository/AdmRepository.java
+++ src/main/java/com/labweb/agrodoa_backend/repository/AdmRepository.java
@@ -7,9 +7,9 @@ import com.labweb.agrodoa_backend.model.pessoas.Administrador;
 import java.util.ArrayList;
 
 public interface AdmRepository extends JpaRepository<Administrador, String>{
-    boolean existsById(String administradorId);
+    boolean existsByIdConta(String idAdministrador);
     ArrayList <Administrador> findAll();  //para o sobre
-    void removeById(String administradorId);
+    void removeByIdConta(String idAdministrador);
 
     //colocando só o Id em vez de AdministradorId ou ContaId pq o spring reconhece Id como padrão e no nosso caso é algo herdado
 }
