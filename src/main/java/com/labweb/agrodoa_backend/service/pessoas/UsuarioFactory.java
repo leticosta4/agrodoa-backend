79456090834bec1d2b0515fbe478b196d42253af
let <leticiacostaoa@gmail.com>
2025-06-28 13:43:54 -0300

    só adicionando a logica pensada p aplicação dos padrões

--- src/main/java/com/labweb/agrodoa_backend/service/pessoas/UsuarioFactory.java
+++ src/main/java/com/labweb/agrodoa_backend/service/pessoas/UsuarioFactory.java
@@ -3,3 +3,13 @@ package com.labweb.agrodoa_backend.service.pessoas;
 public class UsuarioFactory {
     //criar objs de usuario aqui
 }
+
+/**
+ * logica fatory que pensei por cima
+ * product > usuario
+ * concrete products > fornecedor, usuario e hibrido
+ * 
+ * fica faltando um creator e talvez concrete creators, fiquei na dúvida onde seria (talvez o controller) mas é o creator que instancia a classe de factory
+ * 
+ * a logica do template method seria basicamente pq em usuario tem alguns metodos padrão e nas classes filhas outras de acordo com seu comportamento
+ */
