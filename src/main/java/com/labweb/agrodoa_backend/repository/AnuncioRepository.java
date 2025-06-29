996456ef0dbb92c0365960d243536d6de21261f6
verttb <alyssonoliveira456@gmail.com>
2025-06-29 16:22:43 -0300

    refactor: Adicionando specification

--- src/main/java/com/labweb/agrodoa_backend/repository/AnuncioRepository.java
+++ src/main/java/com/labweb/agrodoa_backend/repository/AnuncioRepository.java
@@ -5,26 +5,24 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import com.labweb.agrodoa_backend.model.Anuncio;
 import com.labweb.agrodoa_backend.model.enums.TipoAnuncio;
 
-public interface AnuncioRepository extends JpaRepository<Anuncio, String>{
+public interface AnuncioRepository extends JpaRepository<Anuncio, String>, JpaSpecificationExecutor<Anuncio>{
     boolean existsByIdAnuncio(String idAnuncio);
-    ArrayList <Anuncio> findAllBy();
+    ArrayList <Anuncio> findAll();
     Optional<Anuncio> findByIdAnuncio(String idAnuncio);  //usado no acesso a um unico anuncio, para finalizar ou desativar
 
     @Query(value = "SELECT * FROM anuncio WHERE tipo_anuncio = :tipoAnuncio;", nativeQuery = true) //ver aqui como vai associar o item do enum a um caracter 'V' ou 'D'
     ArrayList<Anuncio> findByTipoAnuncio(TipoAnuncio tipoAnuncio); //venda ou doação
-    ArrayList<Anuncio> findByTipoAnuncio(TipoAnuncio tipoAnuncio); //venda ou doação
 
     @Query(value = "SELECT * FROM anuncio WHERE LOWER(titulo) LIKE CONCAT('%', :pesquisa, '%');", nativeQuery = true)
     ArrayList<Anuncio> findByTituloContaining(String pesquisa); //barra de pesquisa mas precisa refinar
-    ArrayList<Anuncio> findByTituloContaining(String pesquisa); //barra de pesquisa mas precisa refinar
 
     @Query(value = "SELECT * FROM anuncio WHERE data_expiracao = :dataExpiracao;", nativeQuery = true)
-    ArrayList<Anuncio> findByDataExpiracao(LocalDate dataExpiracao);
     ArrayList<Anuncio> findByDataExpiracao(LocalDate dataExpiracao);
 
 
