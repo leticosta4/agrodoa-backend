178d1695566bc371052ea40d23a303e7cd5beb1d
verttb <alyssonoliveira456@gmail.com>
2025-07-01 11:48:53 -0300

    feat: DTO para anuncios que possuem usuario

---
+++ src/main/java/com/labweb/agrodoa_backend/dto/anuncio/AnuncioFiltroUsuarioDTO.java
@@ -0,0 +1,11 @@
+package com.labweb.agrodoa_backend.dto.anuncio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnuncioFiltroUsuarioDTO extends AnuncioFiltroDTO {
+    private String idAnunciante;
+
+}
