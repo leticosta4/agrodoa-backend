563d07371c259a0f177d1af7e318ac4e9dae4a55
verttb <alyssonoliveira456@gmail.com>
2025-07-01 10:56:07 -0300

    feat: data,precomax, status e tipo validados

--- src/main/java/com/labweb/agrodoa_backend/dto/anuncio/AnuncioFiltroDTO.java
+++ src/main/java/com/labweb/agrodoa_backend/dto/anuncio/AnuncioFiltroDTO.java
@@ -1,6 +1,13 @@
 package com.labweb.agrodoa_backend.dto.anuncio;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
+import com.labweb.agrodoa_backend.model.enums.StatusAnuncio;
+import com.labweb.agrodoa_backend.model.enums.TipoAnuncio;
 
 import lombok.Getter;
 
@@ -8,7 +15,32 @@ import lombok.Getter;
 public class AnuncioFiltroDTO {
     private String nome;
     private String cidade;
+
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
     private BigDecimal precoMin;
+
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    private BigDecimal precoMax;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate dataExpiracao;
+
     private String tipo;
-    //adicionar o de data expiração que a gente já tinha no repository e um de preço maximo seria bom talvez
+    private String status;
+
+     public TipoAnuncio getTipoEnum() {
+        try {
+            return tipo == null ? null : TipoAnuncio.valueOf(tipo.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+     public StatusAnuncio getStatusEnum() {
+        try {
+            return status == null ? null : StatusAnuncio.valueOf(status.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return StatusAnuncio.ATIVO;
+        }
+    }
 }
