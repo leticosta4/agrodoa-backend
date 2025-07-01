616aa6d5e0ddb7a58151deec5dacc584b06f3ce7
verttb <alyssonoliveira456@gmail.com>
2025-07-01 12:03:15 -0300

    refactor: Filtro de anuncio por usuario funcional

--- src/main/java/com/labweb/agrodoa_backend/service/anuncios/AnuncioService.java
+++ src/main/java/com/labweb/agrodoa_backend/service/anuncios/AnuncioService.java
@@ -1,12 +1,14 @@
 package com.labweb.agrodoa_backend.service.anuncios;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.labweb.agrodoa_backend.dto.anuncio.AnuncioFiltroDTO;
+import com.labweb.agrodoa_backend.dto.anuncio.AnuncioFiltroUsuarioDTO;
 import com.labweb.agrodoa_backend.dto.anuncio.AnuncioRespostaDTO;
 import com.labweb.agrodoa_backend.model.Anuncio;
 import com.labweb.agrodoa_backend.repository.AnuncioRepository;
@@ -17,20 +19,44 @@ public class AnuncioService {
     @Autowired
     private AnuncioRepository anuncioRepo;
 
+
+   private Specification<Anuncio> criarSpecification(AnuncioFiltroDTO dto) {
+    return Specification
+        .where(AnuncioSpecification.filtrarPorNome(dto.getNome()))
+        .and(AnuncioSpecification.filtrarPorCidade(dto.getCidade()))
+        .and(AnuncioSpecification.filtrarPorPrecoMin(dto.getPrecoMin()))
+        .and(AnuncioSpecification.filtrarPorPrecoMax(dto.getPrecoMax()))
+        .and(AnuncioSpecification.filtrarPorTipo(dto.getTipoEnum()))
+        .and(AnuncioSpecification.filtrarPorDataExpiracao(dto.getDataExpiracao()))
+        .and(AnuncioSpecification.filtrarPorStatus(dto.getStatusEnum()));
+        
+}
+    
+
     public List<AnuncioRespostaDTO> buscarAnunciosFiltro(AnuncioFiltroDTO dto){
-        Specification<Anuncio> spec = Specification
-                                                .where(AnuncioSpecification.filtrarPorNome(dto.getNome()))
-                                                .and(AnuncioSpecification.filtrarPorCidade(dto.getCidade()))
-                                                .and(AnuncioSpecification.filtrarPorPrecoMin(dto.getPrecoMin()))
-                                                .and(AnuncioSpecification.filtrarPorPrecoMax(dto.getPrecoMax()))
-                                                .and(AnuncioSpecification.filtrarPorTipo(dto.getTipo()))
-                                                .and(AnuncioSpecification.filtrarPorDataExpiracao(dto.getDataExpiracao()))
-                                                .and(AnuncioSpecification.filtrarPorStatus(dto.getStatus()));
+        Specification<Anuncio> spec = criarSpecification(dto);
 
                                                 
         return anuncioRepo.findAll(spec)
                           .stream()
                           .map(AnuncioRespostaDTO::new)
                           .toList();
-    }    
+    }   
+    
+    
+    public Optional<AnuncioRespostaDTO> buscarAnuncioPorId(String anuncioId){
+    
+        return anuncioRepo.findById(anuncioId).map(AnuncioRespostaDTO::new);
+    }
+
+    public List<AnuncioRespostaDTO> buscarAnunciosFiltroUsuario(AnuncioFiltroUsuarioDTO dto){
+        Specification<Anuncio> spec = 
+                                criarSpecification(dto)
+                                .and(AnuncioSpecification.filtrarPorAnuncianteId(dto.getIdAnunciante()));
+
+        return anuncioRepo.findAll(spec)
+                          .stream()
+                          .map(AnuncioRespostaDTO::new)
+                          .toList();
+    }
 }
