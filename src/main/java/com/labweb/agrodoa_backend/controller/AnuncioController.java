e76ea7518577b4b010377601884d720ac004830f
verttb <alyssonoliveira456@gmail.com>
2025-07-01 11:32:24 -0300

    feat: Anuncio por id e por id de usuario

--- src/main/java/com/labweb/agrodoa_backend/controller/AnuncioController.java
+++ src/main/java/com/labweb/agrodoa_backend/controller/AnuncioController.java
@@ -2,29 +2,45 @@ package com.labweb.agrodoa_backend.controller;
 
 import java.util.List;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.labweb.agrodoa_backend.dto.anuncio.AnuncioFiltroDTO;
 import com.labweb.agrodoa_backend.dto.anuncio.AnuncioRespostaDTO;
 import com.labweb.agrodoa_backend.service.anuncios.AnuncioService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/anuncios")
 public class AnuncioController {
 
-
     @Autowired
     private AnuncioService anuncioService;
 
+
+    @GetMapping("/usuario/{idUsuario}")
+    public ResponseEntity<List<AnuncioRespostaDTO>> buscarAnuncioPorUsuario(
+        @PathVariable String idUsuario,
+        @ParameterObject @ModelAttribute AnuncioFiltroDTO filtro){
+
+        List<AnuncioRespostaDTO> anuncios = anuncioService.buscarAnuncioPorUserId(idUsuario);
+        if(anuncios.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(anuncios);
+
+    }
     @GetMapping
-    public ResponseEntity<List<AnuncioRespostaDTO>> listarAnunciosFiltro(@ModelAttribute AnuncioFiltroDTO filtro){
+    public ResponseEntity<List<AnuncioRespostaDTO>> listarAnunciosFiltro(@ParameterObject @ModelAttribute AnuncioFiltroDTO filtro){
         List<AnuncioRespostaDTO> anuncios = anuncioService.buscarAnunciosFiltro(filtro);
         if(anuncios.isEmpty()){
             return ResponseEntity.noContent().build();
@@ -32,4 +48,12 @@ public class AnuncioController {
         return ResponseEntity.ok(anuncios);
        
     }
+
+    @GetMapping("/{idAnuncio}")
+    public ResponseEntity<AnuncioRespostaDTO> buscarId(@PathVariable String idAnuncio) {
+       return anuncioService.buscarAnuncioPorId(idAnuncio)
+                         .map(ResponseEntity::ok)
+                         .orElse(ResponseEntity.notFound().build());
+    }
+    
 }
