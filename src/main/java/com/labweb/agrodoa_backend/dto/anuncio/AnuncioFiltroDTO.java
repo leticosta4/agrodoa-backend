3874c6fc2116e8d9a512d3246501481d75c87e76
verttb <alyssonoliveira456@gmail.com>
2025-07-01 10:30:03 -0300

    refactor: descomentando tipo

--- src/main/java/com/labweb/agrodoa_backend/dto/anuncio/AnuncioFiltroDTO.java
+++ src/main/java/com/labweb/agrodoa_backend/dto/anuncio/AnuncioFiltroDTO.java
@@ -9,6 +9,6 @@ public class AnuncioFiltroDTO {
     private String nome;
     private String cidade;
     private BigDecimal precoMin;
-    //private String tipo;
+    private String tipo;
     //adicionar o de data expiração que a gente já tinha no repository e um de preço maximo seria bom talvez
 }
