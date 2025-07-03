f6343ed75291611805861e9459b4dd60822ae17d
let <leticiacostaoa@gmail.com>
2025-07-03 00:46:53 -0300

    refactor: add arquivo para armazenar secrets

--- src/main/java/com/labweb/agrodoa_backend/AgrodoaBackendApplication.java
+++ src/main/java/com/labweb/agrodoa_backend/AgrodoaBackendApplication.java
@@ -2,8 +2,10 @@ package com.labweb.agrodoa_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secrets.properties")
 public class AgrodoaBackendApplication {
 
 	public static void main(String[] args) {
