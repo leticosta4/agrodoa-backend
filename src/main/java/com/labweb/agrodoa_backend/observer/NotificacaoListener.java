7bf937cd27ebaa0fdc27c46d9fee59ffe2bc56e4
let <leticiacostaoa@gmail.com>
2025-07-09 12:12:55 -0300

    refactor: obsever envia email para users na criação de causa

--- src/main/java/com/labweb/agrodoa_backend/observer/NotificacaoListener.java
+++ src/main/java/com/labweb/agrodoa_backend/observer/NotificacaoListener.java
@@ -1,7 +1,10 @@
 package com.labweb.agrodoa_backend.observer;
 
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -9,49 +12,63 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import com.labweb.agrodoa_backend.model.Causa;
-import com.labweb.agrodoa_backend.model.contas.Administrador;
-import com.labweb.agrodoa_backend.repository.contas.AdministradorRepository;
+import com.labweb.agrodoa_backend.model.contas.Usuario;
+import com.labweb.agrodoa_backend.repository.contas.UsuarioRepository;
 import com.labweb.agrodoa_backend.service.EmailService;
 
 //OBSERVER
 @Component
 public class NotificacaoListener { //depois remodelar provavelmente ja q vão ter outros tipos de notificacao
     @Autowired private EmailService emailService;
-    @Autowired private AdministradorRepository admRepo;
+    @Autowired private UsuarioRepository userRepo;
 
     private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Async //o metodo pode rodar em outra thread
     @EventListener
-    public void aoCriarCausa(CausaCriadaEvent evento){
+    public void aoCriarCausa(CausaCriadaEvent evento){  //em lote p n sobrecarregar
         Causa causaCriada = evento.getCausa();
-        List<Administrador> adms = admRepo.findAll();
+        int tamLote = 20;
+        Pageable paginacao = PageRequest.of(0, tamLote);
+        Page<Usuario> paginaUsers;
 
-        if (adms.isEmpty()) {
-            System.out.println("Nenhum administrador encontrado para notificar.");
-            return;
-        }
+        do {
+            paginaUsers = userRepo.findAll(paginacao);
 
-        //preparando o conteúdo para o email
-        String assunto = "Nova Causa Criada: " + causaCriada.getNome();
-        String corpo = String.format(
-            "Olá, Administrador!\n\nUma nova causa foi cadastrada na plataforma e pode precisar de sua atenção.\n\n" +
-            "ID da Causa: %s\n" +
-            "Nome: %s\n" +
-            "Meta de Arrecadação: R$ %.2f\n" +
-            "Prazo Final: %s\n\n" + //formatar ainda
-            "Por favor, revise-a no painel de causas.\n\n" +
-            "Atenciosamente,\nEquipe Agrodoa.",
-            causaCriada.getIdCausa(),
-            causaCriada.getNome(),
-            causaCriada.getMeta(),
-            causaCriada.getPrazo().format(FORMATADOR_DATA)
-        );
+            if(!paginaUsers.hasContent()){ break; }
 
-        //enviando a todos 
-        for(Administrador adm: adms){
-            emailService.enviarEmail(adm.getEmail(), assunto, corpo);
-            System.out.println("\n\nE-mail de notificação enviado para: " + adm.getEmail() + "\n\n");
-        }
+            informes(paginaUsers, paginacao);
+
+            //preparando o conteúdo para o email
+            String assunto = "Uma nova causa precisa da sua ajuda: " + causaCriada.getNome();
+            String corpo = String.format(
+                "Olá, %s!\n\nUma nova causa foi cadastrada em nossa plataforma e pode ser do seu interesse.\n\n" +
+                "Nome: %s\n" +
+                "Descrição: %s\n",
+                "Prazo final para doações: %s\n\n" +
+                "Sua ajuda pode fazer a diferença! Acesse nosso site para saber mais.\n\n" +
+                "Atenciosamente,\nEquipe Agrodoa.",
+                causaCriada.getNome(),
+                causaCriada.getMeta(),
+                causaCriada.getPrazo().format(FORMATADOR_DATA)
+            );
+
+            //enviando a todos 
+            for(Usuario user: paginaUsers.getContent()){
+                String corpoPersonalizado = String.format(corpo, user.getNome());
+                emailService.enviarEmail(user.getEmail(), assunto, corpoPersonalizado);
+                System.out.println("\n\nE-mail de notificação enviado para: " + user.getEmail() + "\n\n");
+            }
+            paginacao = paginacao.next();
+        }while(paginaUsers.hasNext());
+
+        System.out.println("Envio de notificações para usuários concluído.");
+    }
+
+    private void informes(Page<Usuario> intermedPage, Pageable paginacao){
+        System.out.printf("Processando lote de %d usuários (Página %d de %d)%n",
+                              intermedPage.getNumberOfElements(),
+                              paginacao.getPageNumber() + 1,
+                              intermedPage.getTotalPages());
     }
 }
