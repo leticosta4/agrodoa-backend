389cf1032d10ec3db9c274e61690a45f5ca9154f
let <leticiacostaoa@gmail.com>
2025-07-16 13:56:32 -0300

    fix: a main tava quebrada; cloudinary; produto

--- src/main/java/com/labweb/agrodoa_backend/service/auxiliares/CloudinaryService.java
+++ src/main/java/com/labweb/agrodoa_backend/service/auxiliares/CloudinaryService.java
@@ -4,13 +4,20 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Service
 public class CloudinaryService {
-    private Cloudinary cloudinary;
+
+    private final Cloudinary cloudinary;
+
+    @Autowired
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
 
     public String uploadImagem(MultipartFile file) {
         try {
